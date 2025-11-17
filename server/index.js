9b756abddbe6e5dd19c4c90696ad8e92c6fa2616
valentinelovetalia <valentinelovetalia@gmail.com>
2025-11-16 19:52:09 -0500

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,1 @@
+import express from 'express'; import cors from 'cors'; const app = express(); app.use(cors()); app.get('/',(req,res)=>res.send('API is running')); app.listen(4000,()=>console.log('Server started'));
