7d5f8b0002c109715c4c28978c1e86b48d7e43d9
valentinelovetalia <valentinelovetalia@gmail.com>
2025-11-16 20:04:23 -0500

    Delete server directory

--- server/index.js
+++
@@ -1,1 +0,0 @@
-import express from 'express'; import cors from 'cors'; const app = express(); app.use(cors()); app.get('/',(req,res)=>res.send('API is running')); app.listen(4000,()=>console.log('Server started'));
